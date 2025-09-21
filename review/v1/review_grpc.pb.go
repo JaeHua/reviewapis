092f52f39bebb5a4c80d22b67ccb6fb6859589e0
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2025-09-21 15:01:25 +0800

    init

--- review/v1/review_grpc.pb.go
+++ review/v1/review_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v3.20.1
-// source: api/review/v1/review.proto
+// source: review/v1/review.proto
 
 package v1
 
@@ -363,5 +363,5 @@ var Review_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/review/v1/review.proto",
+	Metadata: "review/v1/review.proto",
 }
--- review/v1/review_http.pb.go
+++ review/v1/review_http.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-http v2.9.0
 // - protoc             v3.20.1
-// source: api/review/v1/review.proto
+// source: review/v1/review.proto
 
 package v1
 
